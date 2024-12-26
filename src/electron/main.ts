31f80726756089f792db6bed5f793ba4fdfe599a
BereketGirma <bereketgirma002@gmail.com>
2024-12-26 07:10:27 -0600

    Readjusting preload
    - fixed issue with location for preload file
    - made a handler for the function

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,17 +1,20 @@
-import {app, BrowserWindow} from 'electron';
+import {app, BrowserWindow, ipcMain} from 'electron';
 import path from 'path';
+import fs from 'fs';
 import { isDev } from './util.js'
+import { getPreloadPath } from './pathResolver.js';
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     webPreferences: {
-      preload: "/preload.cts"
+      preload: path.join(getPreloadPath()),
     }
   });
 
   if(isDev()) {
     mainWindow.loadURL('http://localhost:3000')
   } else {
+    console.log(app.getAppPath())
     mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html'));
   }
 };
@@ -29,5 +32,19 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
+  }
+})
+
+
+ipcMain.handle('check-and-create-file', async () => {
+  const filePath = path.resolve(app.getPath('userData'), 'shift.xlsx');
+
+  if(!fs.existsSync(filePath)) {
+    console.log('Excel file does not exist. Creating...')
+    fs.writeFileSync(filePath, '')
+    return { status: 'created', filePath}
+  } else {
+    console.log('Execl file exists');
+    return { status: 'exists', filePath}
   }
 })
