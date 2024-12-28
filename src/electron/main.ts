d984f32cec9da5107c40dd409705fbc10bfb14df
BereketGirma <bereketgirma002@gmail.com>
2024-12-28 00:01:00 -0600

    Fixing window loading on Mac

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -4,27 +4,49 @@ import fs from 'fs';
 import { isDev } from './util.js'
 import { getPreloadPath } from './pathResolver.js';
 
+let mainWindow: BrowserWindow | null = null;
+
+//Handles creating window
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+  //Initilize window
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
     webPreferences: {
       preload: path.join(getPreloadPath()),
-    }
+    },
+    show: false,
   });
 
+  //Check if on development mode
   if(isDev()) {
-    mainWindow.loadURL('http://localhost:3000')
+    mainWindow.loadURL('http://localhost:3000') //Run from localhost for hot reload purposes
   } else {
-    console.log(app.getAppPath())
-    mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html'));
+    mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html')); //Run from distrubution path
   }
+
+  //Show main window once everything is loaded
+  mainWindow.once('ready-to-show', () => {
+    mainWindow?.show();
+  })
+
+  //Handle the close event on macOS to hide the window instead of closing it
+  mainWindow.on('close', (e) => {
+    if(process.platform === 'darwin') {
+      e.preventDefault(); //Prevent the window from closing
+      mainWindow?.hide(); //Hide the window instead
+    }
+  });
 };
 
 app.whenReady().then(() => {
   createWindow();
-  
+
   app.on('activate', () => {
     if(BrowserWindow.getAllWindows().length === 0){
       createWindow();
+    } else {
+      mainWindow?.show();
     }
   })
 })
