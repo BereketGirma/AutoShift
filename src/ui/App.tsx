85dc69cb5d4f938e6606267dd8ff97a894c6093c
Bereket Girma <bereketgirma002@gmail.com>
2025-01-02 21:51:18 -0600

    Updating navigation in app

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import SplashScreen from './components/SplashScreen';
 import MainContent from './components/MainContent';
+import CalanderContent from './components/CalanderContent'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Container } from '@mui/material';
+import CredentialForm from './components/CredentialForm';
 
 const theme = createTheme({
   palette: {
@@ -19,15 +22,28 @@ const theme = createTheme({
 
 const App: React.FC = () => {
   const [initialized, setInitilized] = useState(false);
+  const [currentPage, setCurrentPage] = useState<"main" | "calander">("main");
+
+  const navigateTo = (page: "main" | "calander") => {
+    setCurrentPage(page)
+  }
 
   return (
     <ThemeProvider theme={theme}>
       {!initialized ? (
         <SplashScreen onInitilizationComplete={() => setInitilized(true)} />
+        ) : currentPage === "main" ? (
+        <MainContent onNavigate={() => navigateTo('calander')}/>
         ) : (
-        <MainContent />
+          <Container className='home'>
+            <CalanderContent onNavigate={() => navigateTo('main')} />
+            <CredentialForm></CredentialForm>
+          </Container>
+          
         )}
+
     </ThemeProvider>
+
   )
 }
 
