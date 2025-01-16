c27d07632366fcd5ca17dd2d10a050c2d3e85886
Bereket Girma <bereketgirma002@gmail.com>
2025-01-15 20:53:57 -0600

    Adding comments

--- src/electron/pathResolver.ts
+++ src/electron/pathResolver.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { app } from "electron"
 import { isDev } from "./util.js"
 
+//Handles the path of the preload file in windows and mac directories
 export function getPreloadPath() {
     return path.join(
         app.getAppPath(),
