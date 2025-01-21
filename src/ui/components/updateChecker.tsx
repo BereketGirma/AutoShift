152819f258e5452bf0c72c249c3cc9f6e0eb6bce
BereketGirma <bereketgirma002@gmail.com>
2025-01-20 18:47:05 -0600

    Adding UI for version history list

--- src/ui/components/updateChecker.tsx
+++ src/ui/components/updateChecker.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button } from '@mui/material';
+import { Box, List, Typography, Button, ListItem, Grid2 } from '@mui/material';
 import { useState, useEffect } from 'react';
 
 interface updateCheckerProps {
@@ -8,14 +8,54 @@ interface updateCheckerProps {
 function UpdateChecker({onNavigate}: updateCheckerProps) {
     const [status, setStatus] = useState("idle");
     const [progress, setProgress] = useState(0);
+    const [isMac, setIsMac] = useState<boolean>(false);
 
-    // let platform: string | null = null;
+    const updateList = [
+        {
+            version: '1.0.0',
+            released: '1/20/2025',
+            url: 'some url'
+        },
+        {
+            version: '0.0.0',
+            released: '1/20/2025',
+            url: 'some url'
+        },
+        {
+            version: '0.0.0',
+            released: '1/20/2025',
+            url: 'some url'
+        },{
+            version: '0.0.0',
+            released: '1/20/2025',
+            url: 'some url'
+        },{
+            version: '0.0.0',
+            released: '1/20/2025',
+            url: 'some url'
+        },{
+            version: '0.0.0',
+            released: '1/20/2025',
+            url: 'some url'
+        },
+    ]
     
     useEffect(() => {
-        // window.electron.invoke('get-platform', (_event: any, response: any) => {
-        //     console.log('Work')
-        //     platform = response.platform
-        // })
+        const getPlatform = async () => {
+            try{
+                const response = await window.electron.invoke('get-platform')
+                if(response.platform.includes('mac')){
+                    setIsMac(true)
+                }
+
+                console.log(isMac)
+            } catch (error) {
+                console.error('Error fetching platform', error)
+            }
+        }
+
+        getPlatform();
+       
 
         window.electron.on('update-status', (_event: any, data: any) => {
             console.log("Working:",data)
@@ -54,59 +94,94 @@ function UpdateChecker({onNavigate}: updateCheckerProps) {
         }
     }
 
-    //For MacOS devices
-    // const openReleasePage = () => {
-    //     console.log(platform)
-    //     const url = 'https://github.com/BereketGirma/AutoShift/releases'
-    //     window.electron.invoke('open-external', url)
-    // }
+    // For MacOS devices
+    const openReleasePage = () => {
+        const url = 'https://github.com/BereketGirma/AutoShift/releases'
+        window.electron.invoke('open-external', url)
+    }
 
     return(
         <div className='home'>
-            <Box>
-                <Button onClick={onNavigate}>
-                    Back to MainMenu
-                </Button>
-
-                    <Typography variant='body1' color='black'>
-                        Update app from web
+            <Grid2 container spacing={2}>
+                <Grid2 size={12}>
+                    <Box display={'flex'} justifyContent={'flex-start'}>
+                        <Button variant="contained" onClick={onNavigate} >
+                            Back
+                        </Button>
+                    </Box>
+                    
+                    <Typography variant='h6' color='black'>
+                        Version History
                     </Typography>
+                </Grid2>
 
-                {status === 'downloading' && (
-                    <Typography color='black'>
-                        Progress: {(progress * 10).toFixed(2)}%
-                    </Typography>
-                )}
+                <Grid2 size={12}>
+                    {isMac ? (
+                        <Box
+                            display={'flex'}
+                            flexDirection={'column'}
+                            sx={{ overflow: 'auto', border: '1px solid #ddd', borderRadius:'1em', position:'relative'}}
+                        >
+                            
+                            <List>
+                                {updateList.map((update, index) => 
+                                    <ListItem key={index} sx={{ borderBottom: '1px solid #ddd'}}>
+                                        <Box>
+                                            <Typography variant='body1' color='black' fontWeight='bold'>Version: {update.version}</Typography>
+                                            <Typography variant='body2' color='black'>Released: {update.released}</Typography>
+                                        </Box>
+                                    </ListItem>
+                                )}
+                            </List>  
 
-                <Button onClick={checkForUpdates} variant='contained'>
-                    Check for Updates
-                </Button>
+                                                
+                        </Box>
+                    ) : (
+                        <Box>
+                            <Button onClick={checkForUpdates} variant='contained'>
+                                Check for Updates
+                            </Button>
 
-                {status === 'update-available' && (
-                    <Button onClick={startDownload} variant='contained'>
-                        Download Update
-                    </Button>
-                )}
+                            {status === 'downloading' && (
+                                <Typography color='black'>
+                                    Progress: {(progress * 10).toFixed(2)}%
+                                </Typography>
+                            )}
 
-                {status === 'downloaded' && (
-                    <Button onClick={quitAndInstall} variant='contained'>
-                        Install Update
-                    </Button>
-                )}
+                            {status === 'update-available' && (
+                                <Button onClick={startDownload} variant='contained'>
+                                    Download Update
+                                </Button>
+                            )}
 
-                {status === 'no-updates' && (
-                    <Typography variant='body1'>
-                        No Updates available. Your app is up to date!
-                    </Typography>
-                )}
+                            {status === 'downloaded' && (
+                                <Button onClick={quitAndInstall} variant='contained'>
+                                    Install Update
+                                </Button>
+                            )}
 
-                {status === 'error' && (
-                    <Typography variant='body1' color='error'>
-                        An error occured while checking for updates. Please try again later.
-                    </Typography>
-                )}
-            </Box>
+                            {status === 'no-updates' && (
+                                <Typography variant='body1'>
+                                    No Updates available. Your app is up to date!
+                                </Typography>
+                            )}
 
+                            {status === 'error' && (
+                                <Typography variant='body1' color='error'>
+                                    An error occured while checking for updates. Please try again later.
+                                </Typography>
+                            )}
+                        </Box>
+                    )}
+
+                </Grid2>
+                
+                
+                    
+
+                
+            </Grid2>
+            
         </div>
     )
 }
